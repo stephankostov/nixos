afabaa8359bcf8ea174b75106d3460106631ea1b
steph <stephank179@gmail.com>
2025-03-14 16:07:04 +0000

    first commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+	inherit system;
+	config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+	nixos = lib.nixosSystem {
+	  inherit system;
+	  modules = [ ./configuration.nix home-manager.nixosModules.home-manager {
+	      home-manager.useGlobalPkgs = true;
+	      home-manager.useUserPackages = true;
+              home-manager.users.steph = { imports = [ ./home.nix ]; }; }
+	  ];
+	};
+      };
+      homeConfigurations = {
+	nixos = home-manager.lib.homeManagerConfiguration {
+	  inherit system pkgs;
+	  username = "steph";
+	  homeDirectory = "/home/steph";
+	  configuration = {
+	    imports = [
+	     ./home.nix
+	    ];
+	  };
+	};
+      };
+    };
+}
