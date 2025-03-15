5b8c52ccafdf47dce3c942de6f1513658325c499
Stephan Kostov <77614679+stephankostov@users.noreply.github.com>
2025-03-15 20:53:31 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.device = "nodev";
-  boot.loader.efi.efiSysMountPoint = "/boot"; ; # Assuming /mnt/boot is mounted during installation
+  boot.loader.efi.efiSysMountPoint = "/boot"; # Assuming /mnt/boot is mounted during installation
   # Define on which hard drive you want to install Grub.
   # boot.loader.grub.device = "/dev/nvme0n1p6"; # or "nodev" for efi only
 
