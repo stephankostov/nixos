215717128470211659c38e33e1a878307a16e220
Stephan Kostov <stephank179@gmail.com>
2025-03-16 19:42:50 +0000

    cuda stuff

--- configuration.nix
+++ configuration.nix
@@ -159,7 +159,13 @@
 
   nix = {
     extraOptions = "experimental-features = nix-command flakes";
+    settings = {
+      substituters = [ "https://cuda-maintainers.cachix.org" ];
+      trusted-public-keys = [  "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
+    };
   };
+
+  hardware.nvidia-container-toolkit.enable = true;
 
 }
 
