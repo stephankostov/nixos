0e98a8c87e4158b0483565f0fea50d52e7982fb9
Stephan Kostov <77614679+stephankostov@users.noreply.github.com>
2025-03-16 15:59:52 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,7 @@
   boot.loader.grub.device = "nodev";
   boot.loader.efi.efiSysMountPoint = "/boot"; # Assuming /mnt/boot is mounted during installation
   boot.loader.grub.useOSProber = true;
-  boot.loader.timeout = 10;
+  boot.loader.timeout = 7;
 
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
