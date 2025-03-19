a354f6afed1503b3e0383814875b95190c22daeb
stephankostov <stephank179@gmail.com>
2025-03-19 10:57:07 +0000

    disabling vscode server setting

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       }
       vscode-server.nixosModules.default {
           services.vscode-server.enable = true;
-          services.vscode-server.enableFHS = true;
+          # services.vscode-server.enableFHS = true;
       }
 	  ];
 	};
