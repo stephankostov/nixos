435af9189e7dcc7caadd3871dd51ae461ba3ca06
Stephan Kostov <stephank179@gmail.com>
2025-03-19 09:17:22 +0000

    formatting

--- modules/devenv.nix
+++ modules/devenv.nix
@@ -1,12 +1,16 @@
 { config, lib, pkgs, ... }:
 {
+
   config = {
+
     environment.systemPackages = with pkgs; [ 
       devenv
       direnv
       starship
     ];
-  };
 
-  programs.starship.enable = true;
+    programs.starship.enable = true;
+
+  };
+  
 }
