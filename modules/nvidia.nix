5f3b4c398742694e9c0e90b66bc0e081588d389c
stephankostov <stephank179@gmail.com>
2025-06-23 11:03:15 +0100

    undervolting gpu
    note: we run command
    `sudo nvidia-smi -lgc 1750,1750`

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -20,20 +20,16 @@
       };
 
       nvidia = {
-
         # Modesetting is required.
         modesetting.enable = true;
-
         # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
         # Enable this if you have graphical corruption issues or application crashes after waking
         # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
         # of just the bare essentials.
-        powerManagement.enable = false;
-
+        powerManagement.enable = true;
         # Fine-grained power management. Turns off GPU when not in use.
         # Experimental and only works on modern Nvidia GPUs (Turing or newer).
         powerManagement.finegrained = false;
-
         # Use the NVidia open source kernel module (not to be confused with the
         # independent third-party "nouveau" open source driver).
         # Support is limited to the Turing and later architectures. Full list of 
@@ -42,14 +38,12 @@
         # Only available from driver 515.43.04+
         # Currently alpha-quality/buggy, so false is currently the recommended setting.
         open = false;
-
+        nvidiaPersistenced = true;
         # Enable the Nvidia settings menu,
         # accessible via `nvidia-settings`.
         nvidiaSettings = true;
-
         package = config.boot.kernelPackages.nvidiaPackages.stable; # Use 'stable' for modern GPUs.
       };
-
     };
 
     environment.variables = {
