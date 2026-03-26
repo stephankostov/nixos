c7951c74c0666145bc5750c8977a9def8af5323b
Stephan Kostov <stephank179@gmail.com>
2026-03-26 19:43:58 +0000

    rearranging for multiple hosts

--- flake.nix
+++ flake.nix
@@ -2,57 +2,77 @@
   description = "";
 
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
-  outputs = { self, nixpkgs, home-manager, vscode-server }:
-    let 
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-	inherit system;
-	config.allowUnfree = true;
-      };
+  outputs = { self, nixpkgs, home-manager, vscode-server, ... }:
+    let
       lib = nixpkgs.lib;
+
+      mkHost = {
+        hostName,
+        system,
+        extraModules ? [ ],
+        homeImports ? [ ./home.nix ],
+      }: lib.nixosSystem {
+        inherit system;
+        modules =
+          [
+            ./hosts/${hostName}/configuration.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.steph = { imports = homeImports; };
+            }
+
+            vscode-server.nixosModules.default
+            {
+              services.vscode-server.enable = true;
+            }
+          ]
+          ++ extraModules;
+      };
+
+      mkHome = { system }: home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        modules = [ ./home.nix ];
+      };
+
     in {
       nixosConfigurations = {
-	nixos = lib.nixosSystem {
-	  inherit system;
-	  modules = [ 
-      ./configuration.nix 
-      ./modules/nvidia.nix
-      ./modules/python.nix
-      ./modules/devenv.nix
-      home-manager.nixosModules.home-manager {
-	      home-manager.useGlobalPkgs = true;
-	      home-manager.useUserPackages = true;
-        home-manager.users.steph = { imports = [ ./home.nix ]; }; 
-      }
-      vscode-server.nixosModules.default {
-          services.vscode-server.enable = true;
-          # services.vscode-server.enableFHS = true;
-      }
-	  ];
-	};
+        pc = mkHost {
+          hostName = "pc";
+          system = "x86_64-linux";
+          extraModules = [
+            ./modules/nvidia.nix
+            ./modules/python.nix
+            ./modules/devenv.nix
+          ];
+        };
+
+        pi = mkHost {
+          hostName = "pi";
+          system = "aarch64-linux";
+          extraModules = [
+            ./modules/python.nix
+            ./modules/devenv.nix
+          ];
+        };
       };
+
       homeConfigurations = {
-	nixos = home-manager.lib.homeManagerConfiguration {
-	  inherit system pkgs;
-	  username = "steph";
-	  homeDirectory = "/home/steph";
-	  configuration = {
-	    imports = [
-	     ./home.nix
-	    ];
-	  };
-	};
+        steph-pc = mkHome { system = "x86_64-linux"; };
+        steph-pi = mkHome { system = "aarch64-linux"; };
       };
     };
 }
