671f29ba3afa6ebf35ed0a518eb5b4c615fdd6d0
stephankostov <stephank179@gmail.com>
2026-03-28 19:09:37 +0000

    working

--- modules/vscode-server.nix
+++ modules/vscode-server.nix
@@ -1,9 +1,12 @@
 { config, lib, pkgs, ... }:
+
 let
   extensions = config.services.vscode-server-extensions;
   cfg = config.services.vscode-server;
-  extensionDir = cfg.installPath;
-  extensionPath = "${extensionDir}/extensions";
+
+  # VSCode Server on the remote typically lives under the user home:
+  #   /home/<user>/.vscode-server/extensions
+  extensionsTarget = "/home/steph/.vscode-server/extensions";
 
   extensionJson = pkgs.vscode-utils.toExtensionJson extensions;
   extensionJsonFile = pkgs.writeTextFile {
@@ -11,24 +14,25 @@ let
     destination = "/share/vscode/extensions/extensions.json";
     text = extensionJson;
   };
-in {
+in
+{
   options.services.vscode-server-extensions = lib.mkOption {
     type = lib.types.listOf lib.types.package;
     default = [];
   };
 
   config = lib.mkIf cfg.enable {
-    home.file = {
-      "${extensionPath}".source = (
-        let
-          subDir = "share/vscode/extensions";
-          combinedExtensionsDrv = pkgs.buildEnv {
-            name = "vscode-extensions";
-            paths = extensions ++ lib.singleton extensionJsonFile;
-          };
-        in
-          "${combinedExtensionsDrv}/${subDir}"
-      );
-    };
+    environment.etc."vscode-server-extensions".source = let
+      subDir = "share/vscode/extensions";
+      combinedExtensionsDrv = pkgs.buildEnv {
+        name = "vscode-extensions";
+        paths = extensions ++ lib.singleton extensionJsonFile;
+      };
+    in "${combinedExtensionsDrv}/${subDir}";
+
+    # Symlink that etc entry into the user’s ~/.vscode-server/extensions
+    systemd.tmpfiles.rules = [
+      "L+ ${extensionsTarget} - - - - /etc/vscode-server-extensions"
+    ];
   };
 }
