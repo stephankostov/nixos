b3d944c67a030d695f826faa84175de959ee136f
stephankostov <stephank179@gmail.com>
2026-05-25 14:22:08 +0100

    auto optimise store

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -37,6 +37,7 @@ in
       substituters = [ "https://cuda-maintainers.cachix.org" ];
       trusted-public-keys = [  "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
       trusted-users = [ "root" "steph" ];
+      auto-optimise-store = true;
     };
     gc = {
       automatic = true;
