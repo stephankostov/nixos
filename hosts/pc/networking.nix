7d2b6aadf1b5d93cb6e9c5b388a6be7d31aee895
stephankostov <stephank179@gmail.com>
2026-06-25 10:42:38 +0100

    moving to office until wifi arrives

--- hosts/pc/networking.nix
+++ hosts/pc/networking.nix
@@ -2,20 +2,20 @@
 {
   networking = {
     networkmanager.enable = true;
-    hostName = "stephs-pc";
-    interfaces = {
-      enp4s0 = {
-        ipv4.addresses = [ {
-          address = "192.168.0.50";
-          prefixLength = 24;
-        } ];
-        wakeOnLan = {
-          enable = true;
-        };
-      };
-    };
-    defaultGateway = "192.168.0.1";
-    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    # hostName = "stephs-pc";
+    # interfaces = {
+    #   enp4s0 = {
+    #     ipv4.addresses = [ {
+    #       address = "192.168.0.50";
+    #       prefixLength = 24;
+    #     } ];
+    #     wakeOnLan = {
+    #       enable = true;
+    #     };
+    #   };
+    # };
+    # defaultGateway = "192.168.0.1";
+    # nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
   };
 
